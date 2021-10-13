b10cbffef13081188576de8e4146d71c346e15f1
Antenh K <antenh.kibret@yahoo.com>
2021-10-13 20:15:20 +0300

    Enable Dark Mode on the site
        - Add base styling for 'h1-h6' and 'a' tags

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,7 +19,6 @@ console.log('hello script setup')
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 30px;
 }
 </style>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -9,18 +9,14 @@ const count = ref(0)
 
 <template>
     <div>
-        <h1>Welcome.</h1>
-        <h2>Name is ...</h2>
+        <h1>Welcome</h1>
+        <h2>To the best</h2>
         <h3>About me <a href="#">#</a></h3>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </div>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
-
 label {
   margin: 0 0.5em;
   font-weight: bold;
