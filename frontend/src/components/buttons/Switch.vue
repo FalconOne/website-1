673457738b3bdb0e98b10499c63a40b5a28ba549
Antenh K <antenh.kibret@yahoo.com>
2021-11-06 14:12:46 +0300

    Add missing files from previous commits  - Added SASS & SASS Loader  - Add Switch btn component

---
+++ frontend/src/components/buttons/Switch.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex justify-between items-center" @click="toggleActive = !toggleActive">
+    <div class="w-16 h-10 flex items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out" :class="{ 'bg-green-400': toggleActive}">
+      <div class="bg-white w-8 h-8 rounded-full shadow-md transform duration-300 ease-in-out" :class="{ 'translate-x-6': toggleActive,}"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+interface props {
+    toggleActive: boolean
+}
+
+const props = withDefaults(defineProps<props>(), {
+    toggleActive: false
+})
+</script>
+<!-- <script>
+export default {
+  data() {
+    return {
+      toggleActive: false,
+    };
+  },
+}
+</script> -->
