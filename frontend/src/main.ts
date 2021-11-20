3fac46b830a188e1e1dd5d7f6164e01e19f6ed62
Antenh K <antenh.kibret@yahoo.com>
2021-11-20 14:44:33 +0300

    Formatting: remove unused and unneccessary js imports

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
 
-import '@fortawesome/fontawesome-free/js/all'
 import './index.css'
 
 const app = createApp(App)
 app.use(router)
-app.use(VueAxios, axios)
 app.mount('#app')
