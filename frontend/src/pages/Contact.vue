10da00c8a9560967ef7e990f5ec469d5324d2e2e
Antenh K <antenh.kibret@yahoo.com>
2021-11-23 12:16:23 +0300

    Bugfix: remove previously added vue-meta & references

--- frontend/src/pages/Contact.vue
+++ frontend/src/pages/Contact.vue
@@ -1,11 +1,6 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 import axios from "axios";
-import { useMeta } from 'vue-meta'
-
-useMeta({ 
-    title: 'Contact Me' 
-})
 
 let first_name = ref(''),
     last_name  = ref(''),
