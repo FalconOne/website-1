546b1229638e7089637efa2383110a988b5192fa
Antenh K <antenh.kibret@yahoo.com>
2021-11-29 18:02:54 +0300

    Feature: NotFound page styling & image

--- frontend/src/pages/NotFound.vue
+++ frontend/src/pages/NotFound.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1>Sorry. This page doesn't exist.</h1>
+    <div class="not-found content">
+        <img src="../assets/icons/error_window_icon_193961.svg" alt="Error: 404.Page Not Found">
+        <h1>Sorry. This page doesn't exist.</h1>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+    .not-found {
+        @apply flex flex-col my-12 text-center rounded-md drop-shadow-lg shadow-lg
+        bg-indigo-200 text-gray-800
+        /* Dark */
+        dark:bg-indigo-600 dark:text-white;
+
+        img {
+            @apply h-28;
+        }
+
+    }
+
+</style>
